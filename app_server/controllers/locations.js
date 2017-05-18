947cf97abf29a29320c483f9442480a204d78fa4
Pablo <pablodelamorav@gmail.com>
2017-05-18 15:11:13 -0500

    Database created

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,13 +1,89 @@
 /* GET para la pagina principal */
 module.exports.homelist = function(req, res){
- res.render('locations-list', { title: 'Casa' });
-};
-/* GET para detalles de un elemento */
-module.exports.locationInfo = function(req, res){
- res.render('location-info', { title: 'Información del Lugar' });
+ res.render('locations-list', {
+   title: 'buscaFut - Busca, Encuentra, Juega!',
+    pageHeader: {
+        title: 'buscaFut',
+        strapline: 'Busca, Encuentra, Juega!'
+    },
+    locations: [{
+     name: 'Anahuac Norte',
+     address: '125 High Street, Reading, RG6 1PS',
+     rating: 3,
+     facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+     distance: '100m'
+    },{
+     name: 'Anahuac Sur',
+     address: '125 High Street, Reading, RG6 1PS',
+     rating: 4,
+     facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+     distance: '200m'
+    },{
+     name: 'Tec CSF',
+     address: '125 High Street, Reading, RG6 1PS',
+     rating: 2,
+     facilities: ['Food', 'Premium wifi'],
+     distance: '250m'
+    }]
+  });
 };
 
-/* GET para agregar un review */
-module.exports.addReview = function(req, res){
- res.render('location-review-form', { title: 'Agregar reseña' });
+/* GET para detalles de un elemento */
+module.exports.locationInfo = function(req, res) {
+    res.render('location-info', {
+        title: 'Starcups',
+        pageHeader: {
+            title: 'Starcups'
+        },
+        sidebar: {
+            context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+            callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+        },
+        location: {
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            coords: {
+                lat: 51.455041,
+                lng: -0.9690884
+            },
+            openingTimes: [{
+                days: 'Monday - Friday',
+                opening: '7:00am',
+                closing: '7:00pm',
+                closed: false
+            }, {
+                days: 'Saturday',
+                opening: '8:00am',
+                closing: '5:00pm',
+                closed: false
+            }, {
+                days: 'Sunday',
+                closed: true
+            }],
+            reviews: [{
+                author: 'Simon Holmes',
+                rating: 5,
+                timestamp: '16 July 2013',
+                reviewText: 'What a great place. I can\'t say enough good things about it.'
+            }, {
+                author: 'Charlie Chaplin',
+                rating: 3,
+                timestamp: '16 June 2013',
+                reviewText: 'It was okay1. Coffee wasn\'t great, but the wifi was fast.'
+            }]
+        }
+    });
+};
+
+
+/* GET para poder agregar rese;as */
+module.exports.addReview = function(req, res) {
+    res.render('location-review-form', {
+        title: 'Agrega Reseña',
+        pageHeader: {
+            title: 'Reseña'
+        }
+    });
 };
---
+++ app_server/models/db.js
@@ -0,0 +1,60 @@
+var mongoose = require( 'mongoose' );
+require('./locations');
+
+var dbURI = 'mongodb://localhost/buscaFut';
+if (process.env.NODE_ENV === 'production') {
+ dbURI = 'mongodb://heroku_kcf08sc9:822mlouumn9g7s1i04f902n4mg@ds127531.mlab.com:27531/heroku_kcf08sc9';
+}
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function () {
+ console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error',function (err) {
+ console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+ console.log('Mongoose disconnected');
+});
+
+
+var readLine = require ("readline");
+if (process.platform === "win32"){
+  var rl = readLine.createInterface ({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.on ("SIGINT", function (){
+    process.emit ("SIGINT");
+  });
+}
+
+
+
+var gracefulShutdown = function (msg, callback) {
+  mongoose.connection.close(function () {
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  });
+};
+
+
+process.once('SIGUSR2', function () {
+  gracefulShutdown('nodemon restart', function () {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+
+
+process.on('SIGINT', function () {
+  gracefulShutdown('app termination', function () {
+    process.exit(0);
+  });
+});
+
+
+process.on('SIGTERM', function() {
+  gracefulShutdown('Heroku app shutdown', function () {
+    process.exit(0);
+  });
+});
