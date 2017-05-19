ab293cd24aacbcbbb2af6c1d81fcd8bd5e4698b6
Pablo <pablodelamorav@gmail.com>
2017-05-19 05:07:18 -0500

    sin modulos

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -4,7 +4,7 @@ var apiOptions = {
  server : "http://localhost:3000"
 };
 if (process.env.NODE_ENV === 'production') {
- apiOptions.server = "https://protected-inlet-65821.herokuapp.com/";
+ apiOptions.server = "https://protected-inlet-65821.herokuapp.com";
 }
 
 var _isNumeric = function (n) {
